6aa63877e4b4ea5b2a397d9eb3d7b3033ccf483e
stefandinca <stefan.dinca07@gmail.com>
2025-11-20 13:31:08 +0200

    tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'royal-blue': '#1A3C6E',
+        'royal-blue-2': '#112F5A',
+        'warm-gold': '#CFAE55',
+        'off-white': '#F4F4F4',
+        'off-off-white': '#F6F5F2',
+        'gray-base': '#333333',
+        'cta-teal': '#2CA58D',
+        'off-blue': '#98B0D4'
+      }
+    }
+  },
+  plugins: []
+}
